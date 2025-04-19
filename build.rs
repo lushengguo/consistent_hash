bf537493e5eed72c1c5c5480b497d91ac821cb82
peter-paker <2206176062@qq.com>
2025-04-19 20:43:48 +0800

    remove rust relative code

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/kv_service.proto")?;
-    tonic_build::compile_protos("proto/consistent_hash.proto")?;
-    Ok(())
-}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module consistent_hash
+
+go 1.24.1
